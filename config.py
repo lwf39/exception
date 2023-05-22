7b7bc20781e696b25ebaa7f3bf63762f0c34dd08
feng <75384977+lwf39@users.noreply.github.com>
2023-05-22 21:14:40 +0800

    Delete config.py

--- config.py
+++
@@ -1,9 +0,0 @@
-user = 'root'
-password = '123456'
-host = '127.0.0.1'
-database = 'exception'
-SQLALCHEMY_DATABASE_URI = "mysql://%s:%s@%s:3306/%s" % (user, password, host,database)
-# SQLALCHEMY_ECHO = True
-# headers = { 'Content-Type': 'application/json', 'charset': 'utf-8' }
-etcd_host = '10.1.186.210'
-aliyun_url = 'https://sls.console.aliyun.com/lognext/project/dth3yunproject/logsearch/dth3yunprojectstore'
